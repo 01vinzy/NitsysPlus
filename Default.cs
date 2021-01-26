60ea0833b1d36e8b6a08cb8a5ff8daa0ed875d86
M1fnbr <minecartgolden9@gmail.com>
2021-01-26 16:24:12 -0500

    Add files via upload

---
+++ Default.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NitsyS
+{
+    [ApiController]
+    [Route("/")]
+    public class Default : ControllerBase
+    {
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Greeting() => Ok("Server is running . . .");
+    }
+}
